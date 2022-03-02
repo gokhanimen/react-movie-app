90aa3bb04d708128e59e11c9c894bfbdfd40f4dc
Gökhan İMEN <70821916+gokhanimen@users.noreply.github.com>
2022-03-02 14:05:46 +0300

    SearchBar oluşturuldu.

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 
 // Hooks
 import {useHomeFetch} from '../hooks/useHomeFetch';
@@ -16,7 +17,7 @@ import {useHomeFetch} from '../hooks/useHomeFetch';
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const {state, loading, error} = useHomeFetch();
+    const {state, loading, error, setSearchTerm} = useHomeFetch();
 
     console.log(state);
     
@@ -29,6 +30,9 @@ const Home = () => {
                     text={state.results[0].overview}
                 />
             : null}
+            
+            <SearchBar setSearchTerm={setSearchTerm} />
+
             <Grid header='Populer Movies'>
                 {state.results.map(movie => (
                     <Thumb
