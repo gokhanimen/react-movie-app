6fa94c5e601ebe3a6becb03991431a5643a76af3
Gökhan İMEN <70821916+gokhanimen@users.noreply.github.com>
2022-03-02 13:28:48 +0300

    Spinner oluşturuldu.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 // Config
 import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
@@ -7,6 +7,7 @@ import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
 import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
+import Spinner from './Spinner';
 
 // Hooks
 import {useHomeFetch} from '../hooks/useHomeFetch';
@@ -41,7 +42,9 @@ const Home = () => {
                         movieId={movie.id}
                     />
                 ))}
-            </Grid> 
+            </Grid>
+
+            <Spinner />
         </>
     );
 }
